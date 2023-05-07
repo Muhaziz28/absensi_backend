f7c427092367c05f75d6429dc120cfef57b6c256
Muhaziz28 <muhammadhaikalaziz28@gmail.com>
2023-05-07 20:24:40 +0700

    add route post jabatan

--- controllers/JabatanController.js
+++ controllers/JabatanController.js
@@ -13,3 +13,21 @@ export const getAllJabatan = async(req, res) => {
         return payload(500, false, error.message, null, res)
     }
 }
+
+export const createJabatan = async (req, res) => {
+    try {
+        const { nama_jabatan, kelas_jabatan, tunjangan_kinerja } = req.body
+        if (nama_jabatan.length > 1) {
+            const checkJabatan = await Jabatan.findOne({ where: { nama_jabatan } })
+            if (checkJabatan) return payload(400, false, "Jabatan already exist", null, res)
+        }
+        const jabatan = await Jabatan.create({
+            nama_jabatan,
+            kelas_jabatan,
+            tunjangan_kinerja
+        })
+        return payload(200, true, "Jabatan created", jabatan, res)
+    } catch (error) {
+        return payload(500, false, error.message, null, res)
+    }
+}
--- routes/JabatanRoute.js
+++ routes/JabatanRoute.js
@@ -1,9 +1,10 @@
 import express from "express"
-import {getAllJabatan} from "../controllers/JabatanController.js"
+import {createJabatan, getAllJabatan} from "../controllers/JabatanController.js"
 import {verifyJwt} from "../middleware/verifyJwt.js";
 
 const router = express.Router()
 
 router.get("/jabatan", verifyJwt, getAllJabatan)
+router.post("/jabatan", verifyJwt, createJabatan)
 
 export default router
