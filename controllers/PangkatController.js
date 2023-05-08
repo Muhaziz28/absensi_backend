c0327758556f12e00a569face4601ccc6a9098e2
Muhaziz28 <muhammadhaikalaziz28@gmail.com>
2023-05-08 11:24:37 +0700

    update pangkat route

--- controllers/PangkatController.js
+++ controllers/PangkatController.js
@@ -3,12 +3,12 @@ import Pangkat from "../models/PangkatModel.js";
 
 export const getAllPangkat = async (req, res) => {
     try {
-        const roles = await Pangkat.findAll({
+        const pangkat = await Pangkat.findAll({
             attributes: {
                 exclude: ["created_at", "updated_at"]
             }
         })
-        return payload(200, true, "All pangkat", roles, res)
+        return payload(200, true, "All pangkat", pangkat, res)
     } catch (error) {
         return payload(500, false, error.message, null, res)
     }
@@ -22,7 +22,7 @@ export const createPangkat = async (req, res) => {
             if (checkPangkat) return payload(400, false, "Pangkat sudah ada", null, res)
         }
         const pangkat = await Pangkat.create({ nama_pangkat })
-        return payload(200, true, "Pangkat created", role, res)
+        return payload(200, true, "Pangkat created", pangkat, res)
     } catch (error) {
         return payload(500, false, error.message, null, res)
     }
@@ -32,7 +32,10 @@ export const updatePangkat = async (req, res) => {
     try {
         const { id } = req.params
         const { nama_pangkat } = req.body
-        const pangkat = await Pangkat.update({ nama_pangkat }, { where: { id } })
+        const pangkat = await Pangkat.update(
+            { nama_pangkat },
+            { where: { id }
+        })
         if (!pangkat) return payload(400, false, "Pangkat not found", null, res)
         return payload(200, true, "Pangkat updated", null, res)
     } catch (error) {
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import RoleRoute from './routes/RoleRoute.js'
 import AuthRoute from './routes/AuthRoute.js'
 import JabatanRoute from './routes/JabatanRoute.js'
+import PangkatRoute from "./routes/PangkatRoute.js";
 import db from './config/Database.js';
 
 dotenv.config();
@@ -17,6 +18,7 @@ app.use(express.json())
 app.use(AuthRoute)
 app.use(RoleRoute)
 app.use(JabatanRoute)
+app.use(PangkatRoute)
 
 app.listen(process.env.APP_PORT, () => {
     console.log(`Server is running on port ${process.env.APP_PORT}`)
