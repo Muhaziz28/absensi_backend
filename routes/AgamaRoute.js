6449643e36bf63085b569c5d139f48b9f40fec8c
Muhaziz28 <muhammadhaikalaziz28@gmail.com>
2023-05-18 20:04:26 +0700

    add verifyjwt on agama route

--- routes/AgamaRoute.js
+++ routes/AgamaRoute.js
@@ -1,11 +1,12 @@
 import express from "express"
 import { createAgama, deleteAgama, getAllAgama, updateAgama } from '../controllers/AgamaController.js'
-
+import { verifyJwt } from "../middleware/verifyJwt.js"
+import { superadmin } from "../middleware/superadmin.js"
 const router = express.Router()
 
 router.get("/agama", getAllAgama)
-router.post("/agama", createAgama)
-router.put("/agama/:id", updateAgama)
-router.delete("/agama/:id", deleteAgama)
+router.post("/agama", verifyJwt, superadmin, createAgama)
+router.put("/agama/:id", verifyJwt, superadmin, updateAgama)
+router.delete("/agama/:id", verifyJwt, superadmin, deleteAgama)
 
 export default router
