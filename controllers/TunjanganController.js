64cbab676f953cf544b46bd8f2a16a1e9a869da3
haikal <haikal@haikals-MacBook-Pro.local>
2023-06-11 17:14:27 +0700

    update query tunjangan

--- controllers/TunjanganController.js
+++ controllers/TunjanganController.js
@@ -8,70 +8,69 @@ import Jabatan from "../models/JabatanModel.js"
 
 export const getMyTunjangan = async (req, res) => {
     try {
-        const { id } = req.user
+        const { id } = req.user;
         const user = await User.findOne({
             where: { id },
             attributes: ["id", "name", "email", "role_id", "jabatan_id", "pangkat_id", "satuan_kerja_id"],
             include: {
                 model: Jabatan,
-                attributes: ["id", "kelas_jabatan", "nama_jabatan", "tunjangan_kinerja"]
-            }
-        })
+                attributes: ["id", "kelas_jabatan", "nama_jabatan", "tunjangan_kinerja"],
+            },
+        });
 
-        if (user.jabatan_id === null) return payload(400, false, "Jabatan belum diatur oleh admin", null, res)
+        if (user.jabatan_id === null) return payload(400, false, "Jabatan belum diatur oleh admin", null, res);
 
-        const month = new Date().getMonth() + 1
-        const year = new Date().getFullYear()
+        const month = new Date().getMonth() + 1;
+        const year = new Date().getFullYear();
         const absenMasuk = await AbsenMasuk.findAll({
             where: [
                 { user_id: id },
-                { created_at: { [Op.gte]: `${year}-${month}-01` } }
+                { created_at: { [Op.gte]: `${year}-${month}-01` } },
             ],
             attributes: {
                 exclude: ["user_id"],
             },
             order: [["created_at", "DESC"]],
-        })
+        });
 
         const absenPulang = await AbsenPulang.findAll({
             where: [
                 { user_id: id },
-                { created_at: { [Op.gte]: `${year}-${month}-01` } }
+                { created_at: { [Op.gte]: `${year}-${month}-01` } },
             ],
             attributes: {
                 exclude: ["user_id"],
             },
             order: [["created_at", "DESC"]],
-        })
+        });
 
         // hitung keterlambatan absen masuk pada bulan ini
-        // total keterlambatan dihitung perhari, 
+        // total keterlambatan dihitung perhari,
         // setiap terlambat 1 - 2 jam akan dikenakan potongan 0.75%
-        const konfigurasiAbsensi = await KonfigurasiAbsensi.findOne()
+        const konfigurasiAbsensi = await KonfigurasiAbsensi.findOne();
 
         // tampilkan keterlambatan dan jumlah potongan perhari pada bulan ini
-        const keterlambatan = absenMasuk.map((item, index) => {
-            const jamKeterlambatan = new Date(item.keterlambatan).getHours()
-            const menitKeterlambatan = new Date(item.keterlambatan).getMinutes()
+        const keterlambatan = absenMasuk.map((item) => {
+            const jamKeterlambatan = new Date(item.keterlambatan).getHours();
+            const menitKeterlambatan = new Date(item.keterlambatan).getMinutes();
 
-            // jika keterlambatan lebih dari 2 jam, maka potongan 0.75% 
-            // jika lebih dari 2 jam, maka potongan akan ditambah 0.75 perdua jam
-            const potongan = jamKeterlambatan > 2 ? 0.75 + (jamKeterlambatan - 2) * 0.75 : 0.75
+            // hanya user yang terlambat yang akan dikenakan potongan
+            const potongan = jamKeterlambatan > 2 ? 0.75 + (jamKeterlambatan - 2) * 0.75 : 0;
 
             return {
                 tanggal: new Date(item.created_at).toLocaleDateString(),
                 potongan,
-            }
-        })
+            };
+        });
 
         // hitung jumlah potongan keterlambatan
         const totalPotonganKeterlambatan = keterlambatan.reduce((acc, item) => {
-            return acc + item.potongan
-        }, 0)
+            return acc + item.potongan;
+        }, 0);
 
-        const totalTunjanganAwal = user.jabatan.tunjangan_kinerja
-        const totalPotonganKeterlambatanPersen = totalPotonganKeterlambatan / 100
-        const totalTunjanganAkhir = totalTunjanganAwal - (totalTunjanganAwal * totalPotonganKeterlambatanPersen)
+        const totalTunjanganAwal = user.jabatan.tunjangan_kinerja;
+        const totalPotonganKeterlambatanPersen = totalPotonganKeterlambatan / 100;
+        const totalTunjanganAkhir = totalTunjanganAwal - totalTunjanganAwal * totalPotonganKeterlambatanPersen;
 
         return payload(200, true, "Data tunjangan berhasil didapatkan", {
             user,
@@ -80,10 +79,9 @@ export const getMyTunjangan = async (req, res) => {
             keterlambatan,
             totalPotonganKeterlambatan,
             totalTunjanganAwal,
-            totalTunjanganAkhir
-        }, res)
-
+            totalTunjanganAkhir,
+        }, res);
     } catch (error) {
-        return payload(500, false, error.message, null, res)
+        return payload(500, false, error.message, null, res);
     }
-}
+};
