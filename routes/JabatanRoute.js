e61d2d1be52e580724fdfa4a5770787bd75da451
haikalaziz <muhammadhaikalaziz28@gmail.com>
2023-07-01 14:31:50 +0700

    change middlware on jabatan route

--- routes/JabatanRoute.js
+++ routes/JabatanRoute.js
@@ -2,12 +2,13 @@ import express from "express"
 import { createJabatan, deleteJabatan, getAllJabatan, updateJabatan } from "../controllers/JabatanController.js"
 import { verifyJwt } from "../middleware/verifyJwt.js";
 import { superadmin } from "../middleware/superadmin.js";
+import { keuangan } from "../middleware/keuangan.js";
 
 const router = express.Router()
 
 router.get("/jabatan", verifyJwt, getAllJabatan)
-router.post("/jabatan", verifyJwt, superadmin, createJabatan)
-router.put("/jabatan/:id", verifyJwt, superadmin, updateJabatan)
-router.delete("/jabatan/:id", verifyJwt, superadmin, deleteJabatan)
+router.post("/jabatan", verifyJwt, keuangan, createJabatan)
+router.put("/jabatan/:id", verifyJwt, keuangan, updateJabatan)
+router.delete("/jabatan/:id", verifyJwt, keuangan, deleteJabatan)
 
 export default router
